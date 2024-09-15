439904b79878238aa4e4f280870e38fc5774db38
Aleks B <ajrberezowski@gmail.com>
2024-09-14 20:46:55 -0600

    Add framework

--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ package org.example;
 //https://www.baeldung.com/spark-framework-rest-api
 //https://www.youtube.com/watch?v=9ntKSLLDeSs
 
+import org.json.JSONObject;
+import spark.Response;
+import spark.Response.*;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -14,50 +18,187 @@ import java.util.HashMap;
 import static spark.Spark.*;
 
 public class Main {
+    private static ResultSet getQuery(String query) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/login_schema?allowPublicKeyRetrieval=true&useSSL=false",
+                "username",
+                "password"
+        );
+
+        Statement statement = connection.createStatement();
+        return statement.executeQuery(query);
+    }
+
     public static void main(String[] args) {
         get("/hello", (req, res)->"Hello, world");
 
-        get("/hello/:name", (req,res)->{
-            return "Hellon't, "+ req.params(":name");
+        get("/hello/:name/:name2", (req,res)->{
+            return "Hellon't, " + req.params(":name") + req.params(":name2");
         });
 
         get("/json", (req,res)->{
-            HashMap<String, Double> arrayList = new HashMap<>();
-            arrayList.put("Test", 5.0);
-            arrayList.put("Test1", -5.0);
-            arrayList.put("Test2", 65.0);
-            return arrayList;
+            // response.type("application/json");
+            // Response response = new Response();
+
+            JSONObject jo = new JSONObject();
+            jo.put("data", "someData");
+            jo.put("status", 200);
+            return jo;
         });
 
         get("/sql", (req, res)->{
-            String returnString = "";
+            JSONObject returnObject = new JSONObject();
 
             try{
-                Class.forName("com.mysql.jdbc.Driver");
+                ResultSet resultSet = getQuery("SELECT * FROM USERS");
 
-                Connection connection = DriverManager.getConnection(
-                        "jdbc:mysql://127.0.0.1:3306/login_schema?allowPublicKeyRetrieval=true&useSSL=false",
-                        "read",
-                        "password1"
-                );
-
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
-
+                ArrayList<JSONObject> returnArray = new ArrayList<>();
                 while( resultSet.next() ){
+                    JSONObject jo = new JSONObject();
                     String username = resultSet.getString("username");
                     String password = resultSet.getString("password");
+                    jo.put("username", username);
+                    jo.put("password", password);
 
-                    returnString += username + " ";
-
-                    System.out.println(username);
-                    System.out.println(password);
+                    returnArray.add(jo);
                 }
 
-                return returnString;
+                returnObject.put("data", returnArray);
+
+                return returnObject;
             } catch (Exception e){
                 return e.toString();
             }
+        });
+
+        get("/testParams", "application/json", (req,res)->{
+            // http://localhost:4567/testParams?id=XYZ&id2=A
+
+            String id = req.queryParams("id");
+            String id2 = req.queryParams("id2");
+            return "HI " + id + " " + id2;
+        });
+
+        get("/getGameList", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String filter = req.queryParams("filter");
+            String search = req.queryParams("search");
+
+            return "Not made";
+        });
+
+        get("/getGame", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String game = req.queryParams("game");
+
+            return "Not made";
+        });
+
+        get("/getGameCompletions", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String filter = req.queryParams("filter");
+            String search = req.queryParams("search");
+
+            return "Not made";
+        });
+
+        get("/getGameCompletion", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String game = req.queryParams("game");
+
+            return "Not made";
+        });
+
+        get("/getGameCompletionCategories", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+
+            return "Not made";
+        });
+
+        get("/updateGameCompletionCategoryName", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String oldName = req.queryParams("oldName");
+            String newName = req.queryParams("newName");
+
+            return "Not made";
+        });
+
+        get("/updateGameCompletionCategoryColour", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String name = req.queryParams("name");
+            String colour = req.queryParams("colour");
+
+            return "Not made";
+        });
+
+        get("/updateGameCompletionBuiltin", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String game = req.queryParams("colour");
+            String completionCategory = req.queryParams("completionCategory");
+
+            return "Not made";
+        });
+
+        get("/updateGameCompletionCustom", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String game = req.queryParams("colour");
+            String completionCategory = req.queryParams("completionCategory");
+
+            return "Not made";
+        });
+
+        get("/getGameAchievements", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String game = req.queryParams("game");
+
+            return "Not made";
+        });
+
+        get("/getUser", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+
+            return "Not made";
+        });
+
+        get("/getUserScore", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+
+            return "Not made";
+        });
+
+        get("/getUserAchievements", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String game = req.queryParams("game");
+
+            return "Not made";
+        });
+
+        get("/updateUserAchievementState", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            String achievement = req.queryParams("achievement");
+            String unlocked = req.queryParams("game");
+
+            return "Not made";
+        });
+
+        get("/getUserTimelineEntries", "application/json", (req,res)->{
+            String authToken = req.queryParams("auth");
+            String username = req.queryParams("username");
+            
+            return "Not made";
         });
     }
 }
